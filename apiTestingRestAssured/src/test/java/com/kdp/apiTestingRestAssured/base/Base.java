695bc87fa87efbd1e8b74ade2364d80b24d1b4c7
kuldeep kumar <kuldeep.kdp@gmail.com>
2019-06-12 16:56:30 +0530

    Added comment

--- apiTestingRestAssured/src/test/java/com/kdp/apiTestingRestAssured/base/Base.java
+++ apiTestingRestAssured/src/test/java/com/kdp/apiTestingRestAssured/base/Base.java
@@ -4,6 +4,8 @@ import io.restassured.response.Response;
 
 public class Base {
 	
+	//scenario context concept is used to share Response between step definations files
+	
 	public static Response response;
 	public static String baseURI = "https://api.twitter.com/1.1/";
 	
